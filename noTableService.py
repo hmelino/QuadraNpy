6a4a6fd6868f4ecb40aff12531e19c77ab38adb7
hmelino <hmelino@gmail.com>
2020-01-14 03:51:23 +0000

    PC work finished

--- noTableService.py
+++ noTableService.py
@@ -5,14 +5,10 @@ import matplotlib.pyplot as plt
 import collections
 import numpy as np
 db=pickle.load(open("db.pickle","rb"))
-t20=pickle.load(open("chartValues.pickle","rb"))
-t2020=np.array(t20)
 
 service=0
 month={}
 for t in db:
-	
-	object=db[t]
 	if db[t].date:
 		if db[t].loc in ['009','010']:
 			day=db[t].date.date()
@@ -35,45 +31,10 @@ for t in db:
 days=sorted(month.keys())
 daysLegendArr=[d.strftime("%a")[0] for d in days]
 chartValues=[month[d] for d in days]
-
-import numpy as np
-N = 1
-men_means = (20, 35, 30, 35, 27)
-#women_means = (25, 32, 34, 20, 25)
-
-ind = np.arange(len(chartValues)) 
-
-width = 0.35
-ncv=np.array(chartValues)
-plt.bar(ind, ncv, width, label='Service')
-plt.bar(ind+width , t2020, width,label='Women', color="green")
-
-
-plt.ylabel('Scores')
-plt.title('Service charge in Lounge from bills closed before 5PM Jan 2019')
-
-plt.xticks(ind+width/2,daysLegendArr)
-plt.legend(loc='best')
-ax=plt.subplot()
-ax.axvspan(3,4.4, alpha=0.4, color='red')
-ax.axvspan(10,11.4, alpha=0.4, color='red')
-ax.axvspan(17,18.4, alpha=0.4, color='red')
-ax.axvspan(24,25.4, alpha=0.4, color='red')
-
-plt.show()
-#plt.plot(chartValues)
-
-plt.show()
 	
 
 print(f'Total service for weekday before 5PM in lounge is {service}')
+print(chartValues)
+print('yh')
 
 
-"""
-weekends for Jan 19
-ax.axvspan(3,4.4, alpha=0.4, color='red')
-ax.axvspan(10,11.4, alpha=0.4, color='red')
-ax.axvspan(17,18.4, alpha=0.4, color='red')
-ax.axvspan(24,25.4, alpha=0.4, color='red')
-"""
-#pickle.dump(chartValues,open("chartValues.pickle","wb"))
