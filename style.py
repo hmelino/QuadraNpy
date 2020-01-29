d675174954f2cdfd91dd4ed3bc8ab6baa52546b3
hmelino <hmelino@gmail.com>
2020-01-29 14:44:26 +0000

    Added custom graph style

---
+++ style.py
@@ -0,0 +1,10 @@
+def ownStyle(mtl):
+    mtl.rcParams['axes.linewidth']=1 #outer line thickness
+    mtl.rcParams['axes.edgecolor']='#5D2E46'
+    mtl.rcParams['axes.facecolor']='#F7EEED'
+    mtl.rcParams['figure.facecolor']='#F7EEED'
+    mtl.rcParams['xtick.color']='#5D2E46'
+    mtl.rcParams['ytick.color']='#5D2E46'
+    mtl.rcParams['axes.labelcolor']='#5D2E46'
+    mtl.rcParams['axes.prop_cycle']="cycler('color',['#35526D','#AD6A6C','#8BB2B5'])"
+    mtl.rcParams['grid.color']='#9E807E'
